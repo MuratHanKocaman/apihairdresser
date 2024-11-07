676b1dfbc57926952c6baaff07cbe4e50cd0e7fe
Murat Han Kocaman <murathankocaman@outlook.com>
2024-11-07 21:58:14 +0300

    swagger settings for cors

--- server/swaggerOptions.js
+++ server/swaggerOptions.js
@@ -1,25 +1,25 @@
 // swaggerOptions.js
-const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerJsdoc = require("swagger-jsdoc");
 
 const options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'API Documentation',
-      version: '1.0.0',
-      description: 'A simple Express API',
+      title: "API Documentation",
+      version: "1.0.0",
+      description: "A simple Express API",
     },
     servers: [
       {
-        url: 'http://localhost:5000', // API'nizin çalıştığı URL
+        url: "https://apihairdresser.onrender.com/", // API'nizin çalıştığı URL
       },
     ],
     components: {
       securitySchemes: {
         BearerAuth: {
-          type: 'apiKey',
-          in: 'header', // Token'ı başlık (header) alanında alacağını belirtiyoruz
-          name: 'Authorization', // Başlığın adı "Authorization" olacak
+          type: "apiKey",
+          in: "header", // Token'ı başlık (header) alanında alacağını belirtiyoruz
+          name: "Authorization", // Başlığın adı "Authorization" olacak
           description: "Bearer token formatında giriniz: `Bearer <token>`",
         },
       },
@@ -30,7 +30,7 @@ const options = {
       },
     ],
   },
-  apis: ['./routes/*.js'], // Route dosyalarının konumu
+  apis: ["./routes/*.js"], // Route dosyalarının konumu
 };
 
 const specs = swaggerJsdoc(options);
