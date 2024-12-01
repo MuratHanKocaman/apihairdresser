06b4cb1e8d6b81084ab7cf7783e173bf6bcee49a
Murat Han Kocaman <murathankocaman@outlook.com>
2024-12-02 01:17:33 +0300

    get all staff method fix

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,14 +1,14 @@
-const User = require('../models/User.js');
-
+const User = require("../models/User.js");
 
 // Kullanıcının kendi profilini görüntüleme
 exports.getProfile = async (req, res, next) => {
   try {
-    const user = await User.findById(req.user.userId)
-      .select('-password -createdAt -updatedAt -__v'); // İstenmeyen alanları hariç tut
+    const user = await User.findById(req.user.userId).select(
+      "-password -createdAt -updatedAt -__v"
+    ); // İstenmeyen alanları hariç tut
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     res.status(200).json(user);
@@ -17,7 +17,6 @@ exports.getProfile = async (req, res, next) => {
   }
 };
 
-
 // Kullanıcı profilini güncelleme
 exports.updateProfile = async (req, res, next) => {
   const { name, email, phone } = req.body;
@@ -25,7 +24,7 @@ exports.updateProfile = async (req, res, next) => {
   try {
     const user = await User.findById(req.user.userId);
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     // Sadece name, email ve phone alanlarını güncelle
@@ -36,10 +35,13 @@ exports.updateProfile = async (req, res, next) => {
     await user.save();
 
     // Güncellenmiş kullanıcı bilgisini istenmeyen alanlar olmadan yanıtla
-    const updatedUser = await User.findById(req.user.userId)
-      .select('-password -createdAt -updatedAt -__v');
+    const updatedUser = await User.findById(req.user.userId).select(
+      "-password -createdAt -updatedAt -__v"
+    );
 
-    res.status(200).json({ message: 'Profile updated successfully', user: updatedUser });
+    res
+      .status(200)
+      .json({ message: "Profile updated successfully", user: updatedUser });
   } catch (error) {
     next(error);
   }
@@ -49,7 +51,9 @@ exports.updateProfile = async (req, res, next) => {
 exports.getAllStaff = async (req, res) => {
   try {
     // Role'ü 'staff' olan tüm kullanıcıları getir
-    const staffMembers = await User.find({ role: 'staff' });
+    const staffMembers = await User.find({ role: "staff" }).select(
+      "-password -createdAt -updatedAt -__v"
+    );
     res.status(200).json(staffMembers);
   } catch (error) {
     res.status(500).json({ message: error.message });
